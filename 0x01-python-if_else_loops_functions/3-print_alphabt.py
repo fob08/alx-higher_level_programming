df1dcd84cc821cdf419f13dcc9442d995ac43d7b
olawale <fobadmus@gmail.com>
2022-08-23 10:23:35 -0700

    slso

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for p in range(ord('a'), ord('z')+1):
     if p is not (ord('q')) and p is not (ord('e')):
-        print("{}".format(chr(p)), end = '')
+        print("{}".format(chr(p)), end='')
