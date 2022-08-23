301a5132d70c97a5fb5f6918a4e3fd7a32cf2fc0
olawale <fobadmus@gmail.com>
2022-08-23 10:54:05 -0700

    learnt a new method called ord()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if i in range(ord('a'), ord('z')+1):
+        if ord(i) in range(ord('a'), ord('z')+1):
             i = chr(ord(i) - ord('a') + ord('A'))
             print("{}".format(i), end='')
     print()
