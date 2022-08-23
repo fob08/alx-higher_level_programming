0e2f9451f79bcf07ac992e6736378d003e0625e9
olawale <fobadmus@gmail.com>
2022-08-23 11:30:35 -0700

    sllssl

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        print("{}".format(number % 10))
-    elif number < 0:
-        print("{}".format(number % (-10)))
+    print(abs(number) % 10, end='')
+    return(abs(number) % 10)
 
