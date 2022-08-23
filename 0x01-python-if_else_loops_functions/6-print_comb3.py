7b70150775b18296abdbc6115053d0ce502b6b1f
olawale <fobadmus@gmail.com>
2022-08-23 10:13:23 -0700

    sla

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,10):
-    for i+1 in range(0,10):
-        print(", ".join("{}{}".format(i, i+1)))
+    for j in range((i+1),10):
+        print(", ".join("{}{}".format(i,j)))
+        j++
     i++
