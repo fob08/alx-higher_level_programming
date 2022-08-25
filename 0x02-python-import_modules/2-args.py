c990d62fac5958c63d51520218efcc4554224ee3
olawale <fobadmus@gmail.com>
2022-08-25 14:37:17 -0700

    laolkaa

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    for x in argv:
-        print("{}: {}").format(((argv.index) + 1), x)
+    user_input = argv
+    user_input = user_input.split(",")
+    if len(user_input) == 0:
+        print(".")
+    else:
+        for x in user_input:
+            print("{}: {}").format(((argv.index) + 1), x)
