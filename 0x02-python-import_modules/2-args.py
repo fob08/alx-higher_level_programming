a9f7a88b94896b68afab2b5b71f7df98823222d0
olawale <fobadmus@gmail.com>
2022-08-25 15:11:56 -0700

    lls

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     user_input = argv[1:]
     print("{:d} {:s}{:s}".format((len(user_input)), "arguments" if (len(user_input)) != 1 else "argument", "." if (len(user_input)) == 0 else ":"))
-    for x in user_input:
-        print("{:d}: {:s}").format(((x.index) + 1), x)
+    for x,y in enumerate(user_input):
+        print("{:d}: {:s}".format((x + 1), y))
