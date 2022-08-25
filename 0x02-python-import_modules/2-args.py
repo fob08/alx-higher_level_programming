c70ab42636580c0139618f4df85dd448609d25d0
olawale <fobadmus@gmail.com>
2022-08-25 15:02:15 -0700

    laola

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     user_input = argv[1:]
-    if len(user_input) == 1:
-        print("{} argument.").format(len(user_input))
-    else:
-        print("{} arguments{}").format(len(user_input), if (len(user_input) == 0) & (len(user_input) != 1):
-                .
-            else:
-            :)
-        for x in user_input:
-            print("{}: {}").format(((x.index) + 1), x)
+    print("{:d} {:s}{:s}").format(len(user_input), "arguments" if len(user_input) != 1 else "argument", "." if len(user_input) == 0 else ":")
+    for x in user_input:
+        print("{:d}: {:s}").format(((x.index) + 1), x)
