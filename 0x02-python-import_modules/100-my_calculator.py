bdd503105fb62f51e153e1b78608079694088c2e
olawale <fobadmus@gmail.com>
2022-08-29 12:00:20 -0700

    another calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 from sys import argv
-import calculator_1
+from calculator_1 import add,sub,mul,div
 if __name__ == "__main__"
 input_list = argv
 operator = [+, -, *, /]
@@ -9,19 +9,19 @@ if len(user_input) != 4:
     exit(1)
 else:
     if input_list[2] == operator[0]:
-        result = calculator_1.add(int(input_list[1]), int(input_list[3]))
+        result = add(int(input_list[1]), int(input_list[3]))
         print(result)
         exit(0)
     elif input_list[2] == operator[1]:
-        result = calculator_1.sub(int(input_list[1]), int(input_list[3]))
+        result = sub(int(input_list[1]), int(input_list[3]))
         print(result)
         exit(0)
     elif input_list[2] == operator[2]:
-        result = calculator_1.mul(int(input_list[1]), int(input_list[3]))
+        result = mul(int(input_list[1]), int(input_list[3]))
         print(result)
         exit(0)
     elif input_list[2] == operator[3]:
-        result = calculator_1.div(int(input_list[1]), int(input_list[3]))
+        result = div(int(input_list[1]), int(input_list[3]))
         print(result)
         exit(0)
     else:
