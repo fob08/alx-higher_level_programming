2a09515dc796a44c697dbcba34d1b9cd7a23ae45
fob08 <32003180+fob08@users.noreply.github.com>
2022-08-29 18:55:44 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ =="__main__":
     add = 0
     size = len(sys.argv)
     if size > 1:
-        for i in range(1,size):
+        for i in range(1, size):
             add += (int(sys.argv[i]))
     print("{:d}".format(add))
