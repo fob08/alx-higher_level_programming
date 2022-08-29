832a3c0384327bde53b1042dedce536c0664544d
fob08 <32003180+fob08@users.noreply.github.com>
2022-08-29 19:00:05 +0200

    keep on trying

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     user_input = argv[1:]
-    print("{:d} {:s}{:s}".format((len(user_input)), \n
+    print("{:d} {:s}{:s}".format((len(user_input)), \
                                  "arguments" if (len(user_input)) != 1 else "argument", "." if (len(user_input)) == 0 else ":"))
     for x, y in enumerate(user_input):
         print("{:d}: {:s}".format(x + 1, y))
