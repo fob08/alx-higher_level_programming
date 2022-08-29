f3009707a0b0bccfacaf47bccfac8daad0caacf2
olawale <fobadmus@gmail.com>
2022-08-29 11:57:35 -0700

    another calculator"

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,30 @@
 #!usr/bin/python3
-if __name__ == "__main__"
+from sys import argv
 import calculator_1
-user_input = input()
-input_list = []
+if __name__ == "__main__"
+input_list = argv
 operator = [+, -, *, /]
-input_list = input_list.append(user_input.split(" "))
-if len(input_list) != 3:
+if len(user_input) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
 else:
-    if input_list[1] == operator[0]:
-        result = calculator_1.add(int(input_list[0]), int(input_list[2]))
+    if input_list[2] == operator[0]:
+        result = calculator_1.add(int(input_list[1]), int(input_list[3]))
         print(result)
-        return(0)
-    elif input_list[1] == operator[1]:
-        result = calculator_1.sub(int(input_list[0]), int(input_list[2]))
-    elif input_list[1] == operator[2]:
-        result = calculator_1.mul(int(input_list[0]), int(input_list[2]))
-    elif input_list[1] == operator[3]:
-        result = calculator_1.div(int(input_list[0]), int(input_list[2]))
+        exit(0)
+    elif input_list[2] == operator[1]:
+        result = calculator_1.sub(int(input_list[1]), int(input_list[3]))
+        print(result)
+        exit(0)
+    elif input_list[2] == operator[2]:
+        result = calculator_1.mul(int(input_list[1]), int(input_list[3]))
+        print(result)
+        exit(0)
+    elif input_list[2] == operator[3]:
+        result = calculator_1.div(int(input_list[1]), int(input_list[3]))
+        print(result)
+        exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        return(1)
+        exit(1)
 
