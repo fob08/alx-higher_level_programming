191269516ee45270c4d1ec36897dc7549cd1745f
olawale <fobadmus@gmail.com>
2022-08-30 06:04:25 -0700

    olaoal

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return(tuple(map(lambda i, j: i + j, tuple_a, tuple_b)))
+    a = len(tuple_a)
+    b = len(tuple_b)
+    sums = ((tuple_a[0] if a > 0) + (tuple_b[0] if b > 0),
+            (tuple_a[1] if a > 1) + (tuple_b[0] if b > 1))
+    return(sums)
