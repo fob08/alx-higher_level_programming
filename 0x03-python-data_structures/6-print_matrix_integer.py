a81701a951a344b2ab681e1ffea190d0824e0f64
olawale <fobadmus@gmail.com>
2022-08-30 05:46:05 -0700

    matrix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print("{:d}".format(matrix[::1]))
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=' ')
+        print()
