eedae47edad93b88670116809065bad49ddd89fc
fob08 <32003180+fob08@users.noreply.github.com>
2022-08-30 13:52:56 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    stringg = []
-    for i in my_string:
-        if (ord(i) != ord"(c")) & (ord(i) != ord("C")):
-            stringg.append(i)
-        else:
-            pass
-    return(stringg)
+    return ("".join(i for i in my_string if i not in "Cc"))
