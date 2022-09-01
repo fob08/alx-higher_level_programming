4b43b841d6e47c5216d21b619b859efd0b6ef1be
olawale <fobadmus@gmail.com>
2022-09-01 05:35:46 -0700

    lsols

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is not None:
-        del a_dictionary[key]
+        if key in a_dictionary:
+            del a_dictionary[key]
         return(a_dictionary)
     else:
         Return None
