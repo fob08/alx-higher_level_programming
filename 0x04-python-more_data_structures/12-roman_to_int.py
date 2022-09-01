4541035512ad7b934e0d4f78ab36cc4d393b74d8
fob08 <32003180+fob08@users.noreply.github.com>
2022-09-01 19:03:48 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,14 @@ def roman_to_int(roman_string):
         return 0
     if roman_string == "":
         return 0
-    highest_numb = 0
-    dictionary = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    num = 0
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i, j in zip(roman_string, roman_string[1:]):
-        if i not in dictionary.keys():
+        if i not in dic.keys():
             return 0
-        elif dictionary[i] >= dictionary[j]:
-            highest_numb += dictionary[i]
+        elif dic[i] >= dic[j]:
+            num += dic[i]
         else:
-            highest_numb -= dic[i]
-    highest_num += dictionary[roman_string[-1]]
-    return highest_num
+            num -= dic[i]
+    num += dic[roman_string[-1]]
+    return num
