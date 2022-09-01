ce389fc1b9bb1df3d8f93538ec02ea6c17e62a87
olawale <fobadmus@gmail.com>
2022-09-01 04:07:16 -0700

    lsola

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        for i in my_list:
-            if i != search:
-                pass
-            else:
-                my_list[i] = replace
-        return my_list
+        return([i if i != search else replace for i in my_list])
     return None
