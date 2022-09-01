1dc54941583d415c5f1b87ee10be9f51e2f747ce
olawale <fobadmus@gmail.com>
2022-09-01 04:57:08 -0700

    laola

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary:
-        for key, value in sorted(a_dictionary.items()):
-            print("{}: {}".format(key,value))
-    return None
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key,value))
