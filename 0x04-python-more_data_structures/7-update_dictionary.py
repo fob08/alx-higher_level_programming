de5dce51bc9f9b159e2ab942064f6bd59b6e6852
olawale <fobadmus@gmail.com>
2022-09-01 05:23:40 -0700

    jjj

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
+    if a_dictionary is not None:
         dic2 = {key:value}
         return(a_dictionary.update(dic2))
     else:
