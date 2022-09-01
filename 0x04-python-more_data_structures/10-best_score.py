c13c940811928e7aa738d4ab05458a5fb4f5130c
fob08 <32003180+fob08@users.noreply.github.com>
2022-09-01 17:36:45 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        for i in a_dictionary:
-            if i.value() > (i + 1).value():
-                a = i.key()
-            else:
-                a = (i + 1).key()
-        return  a
-    else:
-        None
+    if a_dictionary:
+        return(max(a_dictionary, key=a_dictionary.get))
